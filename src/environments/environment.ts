01da5cb1e4459b0ee7b14e3c485c38da1e350629
praveendevlp <68875601+praveendevlp@users.noreply.github.com>
2023-11-25 17:46:49 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@
 export const environment = { 
  production: false,
   baseUrl: 'https://api.vazhaiilai.com/api/',
-  imageUrl: 'https://api.vazhaiilai.com/api/storage/images/',
+  imageUrl: 'https://api.vazhaiilai.com/api/storage/app/public/images/',
   appName: 'Vazhaiilai',
 };
